59a2d135734e24bef3f67d2d04e16e599b6a6568
Tachi <tachidito_7inc@hotmail.com>
2024-05-24 14:47:30 -0600

    version 2.5

--- src/Pages/Components/CaliPooApp.jsx
+++ src/Pages/Components/CaliPooApp.jsx
@@ -5,10 +5,10 @@ import Swal from "sweetalert2";
 
 export const CaliPooApp = () => {
     const practicas = [
-        "6: String Concatenation",
-        "7: Calculadora",
-        "8: If-Else by Switch-Case",
-        "9: Estructura de control",
+        "10: de 0 a 100",
+        "11: Movimientos básicos",
+        "12: Movimientos condicionales ",
+        "13: Movimientos mBot con control",
     ];
     const checkboxes = [1, 2, 3, 4];
 
@@ -22,7 +22,7 @@ export const CaliPooApp = () => {
         const typeValue = parseInt(value, 10);
 
         if (!isNaN(typeValue) && typeValue >= 0) {
-            const maxPoints = [20, 20, 25, 20];
+            const maxPoints = [20, 20, 20, 20];
 
             const limitPoints = Math.min(typeValue, maxPoints[idx]);
 
@@ -31,7 +31,7 @@ export const CaliPooApp = () => {
             setValue(newValue);
 
             const points = newValue.reduce((acc, val) => acc + (parseFloat(val) || 0), 0);
-            const total = Math.floor((points / 85) * 40);
+            const total = Math.floor((points / 80) * 40);
             setTotalPoints(total)
         }
     }
