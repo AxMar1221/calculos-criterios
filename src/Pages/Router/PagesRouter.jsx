61053493b1e1a4dcdbb3c5ff6b11a5ed9863a222
Tachi <tachidito_7inc@hotmail.com>
2025-02-23 23:52:44 -0600

    update router

--- src/Pages/Router/PagesRouter.jsx
+++ src/Pages/Router/PagesRouter.jsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from "react-router-dom"
-import { CalCaliApp, CaliPooApp, HomePage } from "../Components"
+import { CalCaliApp, CaliPooApp, HomePage, RandomLink } from "../Components"
 
 export const PagesRouter = () => {
   return (
     <>
         <Routes>
             <Route path='/home' element={<HomePage />}/>
-            <Route path='/links' element={<CalCaliApp />}/>
+            <Route path='/links' element={<RandomLink />}/>
             {/* <Route path='/info' element={<CalCaliApp />}/> */}
             {/* <Route path='/poo' element={<CaliPooApp />}/> */}
 
