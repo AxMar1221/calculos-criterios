20175a085e6a6e94f1aed1629480074c8da1d5c1
Tachi <tachidito_7inc@hotmail.com>
2025-02-24 00:40:32 -0600

    update footer/randomlink change

--- src/Pages/Components/RandomLink.jsx
+++ src/Pages/Components/RandomLink.jsx
@@ -1,9 +1,8 @@
-import { Box, Button, Typography } from '@mui/material';
+import { Box, Button, Card, CardContent, Typography } from '@mui/material';
 import { useState } from 'react'
 
-
 export const RandomLink = () => {
-  
+
   const links = [
     'https://www.google.com',
     'https://www.openai.com',
@@ -24,25 +23,29 @@ export const RandomLink = () => {
   }
 
   return (
-    <Box textAlign={center} mt={5}>
-      <Button variant='contained' color='primary' onClick={handleSelectedLink}>
-        Seleccionar link
-      </Button>
-      { selectedLink && (
-        <>
-        <Typography variant='h6' mt={2}>
-          Link seleccionado: {selectedLink}
-        </Typography>
-        <Button
-          variant='contained'
-          color='secondary'
-          onClick={handleOpenLink}
-          sx={{ mt: 2}}
-        >
-          Abrir link
-        </Button>
-        </>
-      )}
-    </Box>
+    <Card sx={{ bgcolor: '#2252ac', padding: '10px', margin: '15px', borderRadius: '10px' }} elevation={24}>
+      <CardContent>
+        <Box textAlign='center' mt={5}>
+          <Button variant='contained' color='primary' onClick={handleSelectedLink}>
+            Seleccionar link
+          </Button>
+          {selectedLink && (
+            <>
+              <Typography variant='h6' mt={2}>
+                Link seleccionado: {selectedLink}
+              </Typography>
+              <Button
+                variant='contained'
+                color='secondary'
+                onClick={handleOpenLink}
+                sx={{ mt: 2 }}
+              >
+                Abrir link
+              </Button>
+            </>
+          )}
+        </Box>
+      </CardContent>
+    </Card>
   )
 }
--- src/Shared/FooterApp.jsx
+++ src/Shared/FooterApp.jsx
@@ -8,9 +8,9 @@ export const Footer = () => {
     <div className="container">
       <Box className="footer">
         <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-          <img src="https://avatars.githubusercontent.com/u/85906328?v=4" alt="avatar-github" className="avatar-github" />
+          <img src="https://lh3.googleusercontent.com/a-/ALV-UjUmlUVqVFgHIoYdae0Rn3dj9-3XiMpdBgkK-oOQA5FL8Y3KBYw4A5Di8C8twwlX-4Raw3QzstGz9wHjiW8JcVSkIsOkxfLl65iveL8wvWN31pGJ_jzqC76lfgCE2fnkVkPktTxlnvXCXG2lhbKd1nUUwoxwCYNJH2lXC9Hjl9PYN-QK4uU1Ay3e6GvErOA-oaqv4TyxImofZdzB4pDBFt3QfzNUsCMqpw-FSYXey1vqW05wBWiew-0JxdCD4iOjMXo7YeyvL_tdSHFn_wfPP6vP69kjz6-XFOnxFPg1lHafPCC4dQ_aKxIO7Ss_J5sg0JyRJITxlv62XSn4cQVsiFWlPBKmP_mNfEv04DyQbdkoF3FYpxtpbez7qCKlal1eCbCLkK6bW2UtYRnNgci8DotRnFvCz7ZAkvwO7aJSQUmfYvo60iJ8X2bpCS66extsqnxp3sd3HKki0-gbWBhu6-hyNiQ47XlULpGJaVH1_jASvgzQY49OAxaa22-yx_hp4cu8_SsALSNNFx-qQ6UlHrunh8kz5WBdhvjtxwvB_N6LE2-16JZb0o_DcJcmWLXGi9RFLYSn3JLpu4cm5nx8yVT9v_yrxHwn3W5CxfYzerUISfaiBx_s373JHVO0NkbhCBawHS_PAXsYb6v_ULEon7Vh6BCLxfAQhiG2mGVDpA_1c-wpJsp9qBY97GYIMXG4Uhi8L-I4d7HSx4zajwmwOtsPg9RpETnYuSu0ARluE8hoaUOmLKVel0qH4AvMH8D4UvoyMm8tFQcOJ19kimJRmPygTE3IqzHlB4zwGElQ2-IdbbwwV6qqJE3bSTr_II3N2QuFLmnA37jVFhlCi2Mc4u-7-cI2XfnQ-vZ_B9vU_1iTKAtD6wBYoDt03yzdoxVj2zahEnhfauHk41gXrcYNa7oz9SgjeWNrWd-NZhqaxons-JO1YmBs-eohCBx6suArcr49sswa3ERAzz2VqZsTZ7xpBxIm=s96-c-rg-br100" alt="avatar-github" className="avatar-github" />
           <Typography variant="body2" color="white" align="center">
-            {tab}Mario Hernández Profe Tachi
+            {tab}Profe Tachi
           </Typography>
         </Box>
         <Typography variant="body2" color="white" align="center">
